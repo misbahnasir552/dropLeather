050bdf619c47b65e3b259a54fef57e07c7f3a412
abdulhaitekrowe <147368850+abdulhaitekrowe@users.noreply.github.com>
2025-04-15 17:57:18 +0500

    Settlement report

--- src/app/merchant/merchant-portal/settlement-module/settlement-report/page.tsx
+++ src/app/merchant/merchant-portal/settlement-module/settlement-report/page.tsx
@@ -20,6 +20,8 @@ function SettlementReport() {
   const [pageNumber, setPageNumber] = useState(0);
   const envPageSize = process.env.NEXT_PUBLIC_PAGE_SIZE || 10;
   const [totalPages, setTotalPages] = useState<number>(+envPageSize);
+  console.log('totalPages');
+
   const settlementTransactionHistoryTableHeadings: string[] = [
     'Financial Transaction ID',
     'Settlement Date/Time',
