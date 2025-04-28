d0378f9afe2243388dc8cd7c34d073f3942a993d
Misbah Nasir <misbah.nasir@aksa.com.pk>
2025-04-28 19:39:18 +0500

    Cards-fix-final

--- src/app/merchant/home/page.tsx
+++ src/app/merchant/home/page.tsx
@@ -295,15 +295,23 @@ const LoginSucessHome = () => {
       description:
         'All you need is to select payment mode of your integration need and follow step by step integration guide to begin testing ',
       routeName: 'business-nature',
-      hide: userData.onboardingCompleted === true,
+      hide:
+        // (userData.onboardingCompleted === false && userData.onboardingCompleted===false) ||
+
+        (userData.onboardingCompleted === true &&
+          userData.onboardingCompleted === false) ||
+        (userData.onboardingCompleted === true &&
+          userData.onboardingCompleted === true),
     },
     {
       title: 'Request Revision',
       description: 'All you need is to select...',
       // routeName: '/merchant/home/request-revision',
       hide:
-        userData.onboardingCompleted === true &&
-        !userData.isrequestRevision === true,
+        (userData.onboardingCompleted === false &&
+          userData.isrequestRevision === false) ||
+        (userData.onboardingCompleted === true &&
+          userData.isrequestRevision === false),
       onClick: handleRequestRevisionClick,
     },
     {
@@ -312,8 +320,10 @@ const LoginSucessHome = () => {
         'All you need is to select payment mode of your integration need and follow step by step integration guide to begin testing ',
       routeName: '/merchant/merchant-portal/home/',
       hide:
-        userData.onboardingCompleted === true &&
-        !userData.isrequestRevision === false,
+        (userData.onboardingCompleted === false &&
+          userData.isrequestRevision === false) ||
+        (userData.onboardingCompleted === true &&
+          userData.isrequestRevision === true),
     },
     // PLease DO NOT REMOVE THE CODE BELOw
     // {
