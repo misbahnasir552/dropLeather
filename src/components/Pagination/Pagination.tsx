615c090a779fe75abe13399190ce614820b1e534
TELENORBANKPK\aqdas.hassan <aqdas.hassan@telenorbank.pk>
2025-05-16 13:31:23 +0500

    pagination fixed

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -15,31 +15,41 @@ const Pagination = ({
 }: PaginationProps) => {
   return (
     <div className="flex items-center justify-between">
-      <button
-        onClick={onPrev}
-        disabled={pageNumber + 1 === 1}
-        className={`w-28 bg-primary-base text-sm text-screen-white ${
-          pageNumber + 1 === 1
-            ? 'cursor-not-allowed hover:bg-secondary-300'
-            : 'cursor-pointer hover:bg-primary-600'
-        } rounded-md px-4 py-2`}
-      >
-        Previous
-      </button>
+      {totalPages > 1 ? (
+        <button
+          onClick={onPrev}
+          disabled={pageNumber + 1 === 1}
+          className={`w-28 bg-primary-base text-sm text-screen-white ${
+            pageNumber + 1 === 1
+              ? 'cursor-not-allowed hover:bg-secondary-300'
+              : 'cursor-pointer hover:bg-primary-600'
+          } rounded-md px-4 py-2`}
+        >
+          Previous
+        </button>
+      ) : (
+        <div className="w-28" /> // Keeps spacing consistent when button is hidden
+      )}
+
       <span>
         Page {pageNumber + 1} of {totalPages}
       </span>
-      <button
-        onClick={onNext}
-        disabled={pageNumber + 1 === totalPages}
-        className={`w-28 bg-primary-base text-sm text-screen-white ${
-          pageNumber + 1 === totalPages
-            ? 'cursor-not-allowed hover:bg-secondary-300'
-            : 'cursor-pointer hover:bg-primary-600'
-        }  rounded-md px-4 py-2`}
-      >
-        Next
-      </button>
+
+      {totalPages > 1 ? (
+        <button
+          onClick={onNext}
+          disabled={pageNumber + 1 === totalPages}
+          className={`w-28 bg-primary-base text-sm text-screen-white ${
+            pageNumber + 1 === totalPages
+              ? 'cursor-not-allowed hover:bg-secondary-300'
+              : 'cursor-pointer hover:bg-primary-600'
+          } rounded-md px-4 py-2`}
+        >
+          Next
+        </button>
+      ) : (
+        <div className="w-28" />
+      )}
     </div>
   );
 };
